2d6e6b6b0b9c7fefec2b0db44d4d4783a8be4af5
CristRM27 <123188890+CristRM27@users.noreply.github.com>
2023-02-09 12:13:39 -0300

    Partials

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", async function (req, res) {
   return res.send("Home");
 });
 
 app.get("/admin", async function (req, res) {
+  return res.send("Admin");
+});
+
+app.get("/articulos", async function (req, res) {
   return res.send("Admin");
 });
 

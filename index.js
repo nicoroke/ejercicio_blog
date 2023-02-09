c66b759a40487adf2868f72155d53da8f7832c7b
CristRM27 <123188890+CristRM27@users.noreply.github.com>
2023-02-09 12:06:58 -0300

    Routes

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", async function (req, res) {
+  return res.send("Home");
+});
+
+app.get("/admin", async function (req, res) {
+  return res.send("Admin");
+});
+
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));

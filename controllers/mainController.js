f84db8482e7487895a87f1c90df092cd1d1a2c17
CristRM27 <123188890+CristRM27@users.noreply.github.com>
2023-02-11 18:27:12 -0300

    Modularización Rutas y Controllers

---
+++ controllers/mainController.js
@@ -0,0 +1,76 @@
+const { Article } = require("../models");
+const { Sequelize } = require("sequelize");
+
+async function index(req, res) {
+  const articles = await Article.findAll();
+  return res.render("home", { articles });
+}
+
+async function selectArticle(req, res) {
+  const article = await Article.findByPk(req.params.id);
+  return res.render("article", { article });
+}
+
+async function indexAdmin(req, res) {
+  const articles = await Article.findAll();
+  return res.render("admin", { articles });
+}
+
+async function createForm(req, res) {
+  const articles = await Article.findAll();
+  return res.render("crear");
+}
+
+async function createArticle(req, res) {
+  const newArticle = await Article.create({
+    title: `${req.body.title}`,
+    content: `${req.body.content}`,
+    date: {
+      type: Sequelize.DATE,
+      field: "created_at",
+    },
+    image: `${req.body.image}`,
+    author: `${req.body.author}`,
+  });
+  return res.redirect("/admin");
+}
+
+async function editForm(req, res) {
+  const article = await Article.findByPk(req.params.id);
+  res.render("edit", { article });
+}
+
+async function editArticle(req, res) {
+  const newArticle = await Article.update(
+    {
+      title: `${req.body.title}`,
+      content: `${req.body.content}`,
+      date: {
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+      image: `${req.body.image}`,
+      author: `${req.body.author}`,
+    },
+    { where: { id: req.params.id } },
+  );
+  return res.redirect("/admin");
+}
+
+async function deleteArticle(req, res) {
+  const deleteArticle = await Article.destroy({
+    where: { id: req.params.id },
+  });
+  return res.redirect("/admin");
+}
+
+module.exports = {
+  index,
+  selectArticle,
+  indexAdmin,
+  createForm,
+  createArticle,
+  editForm,
+  editArticle,
+  deleteArticle,
+};
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,73 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { Article } = require("../models");
-const { Sequelize } = require("sequelize");
+const mainController = require("../controllers/mainController");
 
-router.get("/", async function (req, res) {
-  const articles = await Article.findAll();
-  return res.render("home", { articles });
-});
-
-router.get("/articulo/:id", async function (req, res) {
-  // console.log("REQ: " + req.params.id);
-  const article = await Article.findByPk(req.params.id);
-  // console.log(article);
-  return res.render("article", { article });
-});
-
-router.get("/admin", async function (req, res) {
-  const articles = await Article.findAll();
-  // console.log(articles);
-  return res.render("admin", { articles });
-});
-
-router.get("/admin/crear", async function (req, res) {
-  const articles = await Article.findAll();
-  // console.log(articles);
-  return res.render("crear");
-});
-
-router.post("/admin", async function (req, res) {
-  const newArticle = await Article.create({
-    title: `${req.body.title}`,
-    content: `${req.body.content}`,
-    date: {
-      type: Sequelize.DATE,
-      field: "created_at",
-    },
-    image: `${req.body.image}`,
-    author: `${req.body.author}`,
-  });
-  return res.redirect("/admin");
-});
-
-router.get("/admin/editar/:id", async function (req, res) {
-  const article = await Article.findByPk(req.params.id);
-  res.render("edit", { article });
-});
-
-router.post("/admin/editar/:id", async function (req, res) {
-  const newArticle = await Article.update(
-    {
-      title: `${req.body.title}`,
-      content: `${req.body.content}`,
-      date: {
-        type: Sequelize.DATE,
-        field: "updated_at",
-      },
-      image: `${req.body.image}`,
-      author: `${req.body.author}`,
-    },
-    { where: { id: req.params.id } },
-  );
-  return res.redirect("/admin");
-});
-
-router.get("/admin/eliminar/:id", async function (req, res) {
-  const deleteArticle = await Article.destroy({
-    where: { id: req.params.id },
-  });
-  return res.redirect("/admin");
-});
+router.get("/", mainController.index);
+router.get("/articulo/:id", mainController.selectArticle);
+router.get("/admin", mainController.indexAdmin);
+router.get("/admin/crear", mainController.createForm);
+router.post("/admin", mainController.createArticle);
+router.get("/admin/editar/:id", mainController.editForm);
+router.post("/admin/editar/:id", mainController.editArticle);
+router.get("/admin/eliminar/:id", mainController.deleteArticle);
 
 module.exports = router;
